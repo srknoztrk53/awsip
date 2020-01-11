d4114df4bc5cbe170aea3e79e734deeed4668057
Serkan <41832490+srknoztrk53@users.noreply.github.com>
2020-01-11 14:50:05 +0300

    Update ip-rotator.sh

--- ip-rotator.sh
+++ ip-rotator.sh
@@ -4,32 +4,32 @@
         mkdir -p /home/ec2-user/logs
         touch /home/ec2-user/logs/$date.log
         
-		echo "---------------------New Run Starts here $(date)----------------" >> /home/ec2-user/logs/$date.log
-		INSTANCE=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+	echo "---------------------New Run Starts here $(date)----------------" >> /home/ec2-user/logs/$date.log
+	INSTANCE=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
         OLD_IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
 		
         echo "This is the INSTANCE ID "$INSTANCE >> /home/ec2-user/logs/$date.log
         echo "THis is the existing IP "$OLD_IP >> /home/ec2-user/logs/$date.log
         
-		#All ELastic IP Delete, Gets Allocation ID for the current IP, trims "" and trims , in the end
+	#All ELastic IP Delete, Gets Allocation ID for the current IP, trims "" and trims , in the end
         ALLOCATION_ID=$(aws ec2 describe-addresses | grep -i AllocationId | cut -f 2 -d ":" | sed 's/"//g' | tr -d "," | head -n 1)
-		echo $(aws ec2 release-address  --allocation-id $ALLOCATION_ID)>> /home/ec2-user/logs/$date.log
-		ALLOCATION_ID=$(aws ec2 describe-addresses | grep -i AllocationId | cut -f 2 -d ":" | sed 's/"//g' | tr -d "," | head -n 1)
-		echo $(aws ec2 release-address  --allocation-id $ALLOCATION_ID)>> /home/ec2-user/logs/$date.log
-		ALLOCATION_ID=$(aws ec2 describe-addresses | grep -i AllocationId | cut -f 2 -d ":" | sed 's/"//g' | tr -d "," | head -n 1)
-		echo $(aws ec2 release-address  --allocation-id $ALLOCATION_ID)>> /home/ec2-user/logs/$date.log
-		ALLOCATION_ID=$(aws ec2 describe-addresses | grep -i AllocationId | cut -f 2 -d ":" | sed 's/"//g' | tr -d "," | head -n 1)
-		echo $(aws ec2 release-address  --allocation-id $ALLOCATION_ID)>> /home/ec2-user/logs/$date.log
-		ALLOCATION_ID=$(aws ec2 describe-addresses | grep -i AllocationId | cut -f 2 -d ":" | sed 's/"//g' | tr -d "," | head -n 1)
-		echo $(aws ec2 release-address  --allocation-id $ALLOCATION_ID)>> /home/ec2-user/logs/$date.log
+	echo $(aws ec2 release-address  --allocation-id $ALLOCATION_ID)>> /home/ec2-user/logs/$date.log
+	ALLOCATION_ID=$(aws ec2 describe-addresses | grep -i AllocationId | cut -f 2 -d ":" | sed 's/"//g' | tr -d "," | head -n 1)
+	echo $(aws ec2 release-address  --allocation-id $ALLOCATION_ID)>> /home/ec2-user/logs/$date.log
+	ALLOCATION_ID=$(aws ec2 describe-addresses | grep -i AllocationId | cut -f 2 -d ":" | sed 's/"//g' | tr -d "," | head -n 1)
+	echo $(aws ec2 release-address  --allocation-id $ALLOCATION_ID)>> /home/ec2-user/logs/$date.log
+	ALLOCATION_ID=$(aws ec2 describe-addresses | grep -i AllocationId | cut -f 2 -d ":" | sed 's/"//g' | tr -d "," | head -n 1)
+	echo $(aws ec2 release-address  --allocation-id $ALLOCATION_ID)>> /home/ec2-user/logs/$date.log
+	ALLOCATION_ID=$(aws ec2 describe-addresses | grep -i AllocationId | cut -f 2 -d ":" | sed 's/"//g' | tr -d "," | head -n 1)
+	echo $(aws ec2 release-address  --allocation-id $ALLOCATION_ID)>> /home/ec2-user/logs/$date.log
 		
         echo "This is the current Allocation ID  ".$ALLOCATION_ID >> /home/ec2-user/logs/$date.log
         echo "Here are the current addresses" >> /home/ec2-user/logs/$date.log
         echo $(aws ec2 describe-addresses) | python -m json.tool >> /home/ec2-user/logs/$date.log
         
-		sleep 3
+	sleep 3
 
-		# Gets new IP and then trims the ""
+	# Gets new IP and then trims the ""
         NEW_IP=$(aws ec2 allocate-address --query PublicIp | tr -d '"')
         echo "here is the new IP" >> /home/ec2-user/logs/$date.log
         echo $NEW_IP >> /home/ec2-user/logs/$date.log
@@ -37,7 +37,7 @@
         echo ''
         echo 'Associating new IP...'
         
-		# Associating the new IP address to the existing instance
+	# Associating the new IP address to the existing instance
         echo "Associating new IP address" >> /home/ec2-user/logs/$date.log
         echo $(aws ec2 associate-address --instance-id $INSTANCE --public-ip $NEW_IP) >> /home/ec2-use$
         echo "Getting IPs addresses again" >> /home/ec2-user/logs/$date.log
