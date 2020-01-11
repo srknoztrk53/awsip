24b55a5d71f0014d4c48b4739a8f7f141c3579d3
Serkan <41832490+srknoztrk53@users.noreply.github.com>
2020-01-11 15:07:30 +0300

    Update ip-rotator.sh

--- ip-rotator.sh
+++ ip-rotator.sh
@@ -3,30 +3,26 @@
         date=$(date '+%Y.%m.%d')
         mkdir -p /home/ec2-user/logs
         touch /home/ec2-user/logs/$date.log
-        
+
 	echo "---------------------New Run Starts here $(date)----------------" >> /home/ec2-user/logs/$date.log
 	INSTANCE=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
         OLD_IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
-		
+
         echo "This is the INSTANCE ID "$INSTANCE >> /home/ec2-user/logs/$date.log
         echo "THis is the existing IP "$OLD_IP >> /home/ec2-user/logs/$date.log
-        
+
 	#All ELastic IP Delete, Gets Allocation ID for the current IP, trims "" and trims , in the end
         ALLOCATION_ID=$(aws ec2 describe-addresses | grep -i AllocationId | cut -f 2 -d ":" | sed 's/"//g' | tr -d "," | head -n 1)
-	echo $(aws ec2 release-address  --allocation-id $ALLOCATION_ID)>> /home/ec2-user/logs/$date.log
+	echo $(aws ec2 release-address  --allocation-id $ALLOCATION_ID)
 	ALLOCATION_ID=$(aws ec2 describe-addresses | grep -i AllocationId | cut -f 2 -d ":" | sed 's/"//g' | tr -d "," | head -n 1)
-	echo $(aws ec2 release-address  --allocation-id $ALLOCATION_ID)>> /home/ec2-user/logs/$date.log
+	echo $(aws ec2 release-address  --allocation-id $ALLOCATION_ID)
 	ALLOCATION_ID=$(aws ec2 describe-addresses | grep -i AllocationId | cut -f 2 -d ":" | sed 's/"//g' | tr -d "," | head -n 1)
-	echo $(aws ec2 release-address  --allocation-id $ALLOCATION_ID)>> /home/ec2-user/logs/$date.log
+	echo $(aws ec2 release-address  --allocation-id $ALLOCATION_ID)
 	ALLOCATION_ID=$(aws ec2 describe-addresses | grep -i AllocationId | cut -f 2 -d ":" | sed 's/"//g' | tr -d "," | head -n 1)
-	echo $(aws ec2 release-address  --allocation-id $ALLOCATION_ID)>> /home/ec2-user/logs/$date.log
+	echo $(aws ec2 release-address  --allocation-id $ALLOCATION_ID)
 	ALLOCATION_ID=$(aws ec2 describe-addresses | grep -i AllocationId | cut -f 2 -d ":" | sed 's/"//g' | tr -d "," | head -n 1)
-	echo $(aws ec2 release-address  --allocation-id $ALLOCATION_ID)>> /home/ec2-user/logs/$date.log
-		
-        echo "This is the current Allocation ID  ".$ALLOCATION_ID >> /home/ec2-user/logs/$date.log
-        echo "Here are the current addresses" >> /home/ec2-user/logs/$date.log
-        echo $(aws ec2 describe-addresses) | python -m json.tool >> /home/ec2-user/logs/$date.log
-        
+	echo $(aws ec2 release-address  --allocation-id $ALLOCATION_ID)
+
 	sleep 3
 
 	# Gets new IP and then trims the ""
@@ -36,7 +32,7 @@
         echo "New IP: $NEW_IP"
         echo ''
         echo 'Associating new IP...'
-        
+
 	# Associating the new IP address to the existing instance
         echo "Associating new IP address" >> /home/ec2-user/logs/$date.log
         echo $(aws ec2 associate-address --instance-id $INSTANCE --public-ip $NEW_IP) >> /home/ec2-use$
